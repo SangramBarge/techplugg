45cd24e3287c82d82cf956b120c833834b2ca9c6
sangram <sangramvbarge@gmail.com>
2019-01-06 10:30:13 +0530

    created new repo

---
+++ src/main/java/com/techplugg/Controller/Rest/MainRestController.java
@@ -0,0 +1,13 @@
+package com.techplugg.Controller.Rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+	@GetMapping(value = "/")
+	public String hello(){
+		return "hello world";
+	}
+}
